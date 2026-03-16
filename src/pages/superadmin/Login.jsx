0259ef8624722db9c4e400fd0ba34ce2daffc611
Abhiram Rangoon <abhiramrangoon@Abhirams-MacBook-Air-3.local>
2026-03-16 16:40:32 +0530

    changes

---
+++ src/pages/superadmin/Login.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import LoginPage from '../LoginPage';
+
+export default function SuperAdminLogin() { return <LoginPage role="superadmin" title="Super Admin" colorClass="bg-superAdmin" />; }
