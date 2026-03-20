93f3951ee33954a6ad590d7f93bcdbc34540b232
Abhiram Rangoon <abhiramrangoon@Abhirams-MacBook-Air-4.local>
2026-03-20 10:34:04 +0530

    removed unwanted files

--- frontend/src/pages/superadmin/Login 3.jsx
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import LoginPage from '../LoginPage';
-
-export default function SuperAdminLogin() { return <LoginPage role="superadmin" title="Super Admin" colorClass="bg-superAdmin" />; }
