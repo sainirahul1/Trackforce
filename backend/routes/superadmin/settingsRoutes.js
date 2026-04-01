0fe3382cca9d6c34c3a5ae7a79edfa9ac94236ac
Chandrashekar0123 <chandrashekar63707@gmail.com>
2026-04-01 15:36:00 +0530

    Connected backend of Super Admin - Settings - Database

--- backend/routes/superadmin/settingsRoutes.js
+++ backend/routes/superadmin/settingsRoutes.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { getSettings, updateSettings, getDatabaseAnalytics } = require('../../controllers/superadmin/settingsController');
+const { 
+  getSettings, 
+  updateSettings, 
+  getDatabaseAnalytics,
+  getDuplicates,
+  cleanupDuplicates
+} = require('../../controllers/superadmin/settingsController');
 const { protect, admin } = require('../../middleware/authMiddleware');
 
 router.use(protect);
@@ -8,6 +14,8 @@ router.use(admin);
 
 router.get('/', getSettings);
 router.get('/analytics', getDatabaseAnalytics);
+router.get('/duplicates', getDuplicates);
+router.post('/cleanup', cleanupDuplicates);
 router.put('/', updateSettings);
 
 module.exports = router;
